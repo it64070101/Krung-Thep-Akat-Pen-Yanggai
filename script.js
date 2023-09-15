365fce20467b85210ca34747254b1e7883e936f5
Mr.Kalamplee <64070101@kmitl.ac.th>
2023-09-15 16:06:38 +0700

    update landing and page 2

---
+++ script.js
@@ -0,0 +1,8 @@
+function disableDust() {
+    document.querySelector('body').style.overflowY = 'visible';
+    document.querySelector('#dust-sect').style.display = 'none';
+}
+
+function setOverflowHidden() {
+    document.querySelector('body').style.overflowY = 'hidden';
+}
