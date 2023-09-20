cad9f819bf1fb7dd2b780ca44cda479987f0ad68
Mr.Kalamplee <64070101@kmitl.ac.th>
2023-09-20 22:27:32 +0700

    200923 REALLY UPDATE

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function disableDust() {
+    document.querySelector('#dust-1').style.width = '0%';
+    document.querySelector('#dust-2').style.width = '0%';
+    document.querySelector('#dust-3').style.width = '0%';
+    document.querySelector('#dust-4').style.width = '0%';
+    document.querySelector('#dust-5').style.width = '0%';
+    document.querySelector('#dust-sect').style.setProperty('animation', 'weather-scroll-animation')
+    document.querySelector('#dust-sect').style.setProperty('animation-delay', '5s')
+    document.querySelector('#dust-sect').style.setProperty('animation-fill-mode', 'forwards')
     document.querySelector('body').style.overflowY = 'visible';
-    document.querySelector('#dust-sect').style.display = 'none';
 }
 
 function setOverflowHidden() {
     document.querySelector('body').style.overflowY = 'hidden';
+}
+
+function scrollToTop() {
+    window.scrollTo(0, 0);
+    console.log("yes")
 }
