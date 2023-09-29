0896e23008ae273fc5e89b9e6fd77e364ce41aee
Mr.Kalamplee <64070101@kmitl.ac.th>
2023-09-29 12:20:32 +0700

    update image size

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ function disableDust() {
     document.querySelector('#dust-3').style.width = '0%';
     document.querySelector('#dust-4').style.width = '0%';
     document.querySelector('#dust-5').style.width = '0%';
-    document.querySelector('#dust-sect').style.setProperty('animation', 'weather-scroll-animation')
-    document.querySelector('#dust-sect').style.setProperty('animation-delay', '5s')
-    document.querySelector('#dust-sect').style.setProperty('animation-fill-mode', 'forwards')
+    // document.querySelector('#dust-sect').style.setProperty('animation', 'weather-scroll-animation')
+    // document.querySelector('#dust-sect').style.setProperty('animation-delay', '5s')
+    // document.querySelector('#dust-sect').style.setProperty('animation-fill-mode', 'forwards')
     document.querySelector('#dust-sect').style.setProperty('opacity', '50%')
     document.querySelector('body').style.overflowY = 'visible';
     document.querySelector('#dust-guide').style.display = 'none';
