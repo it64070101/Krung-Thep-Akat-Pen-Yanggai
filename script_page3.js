52221f9da30e35bc3a5c7a703573635024377f9c
Thanawat sritho <64070044@kmitl.ac.th>
2023-10-05 01:18:24 +0700

    update aqi random and page3
    finish aqi random.html and update aqi random in to page 3

--- script_page3.js
+++ script_page3.js
@@ -3,6 +3,7 @@ let is_animation_playing = false; // Flag to track animation status
 var pos = -35;
 var oldTarget_name = "";
 var oldTarget_num = 88;
+
 function myMove(num) {
     if (is_animation_playing) {
       return; // Don't start a new animation if one is already playing
@@ -102,4 +103,65 @@ function myMove(num) {
         document.getElementById('choose_'+oldTarget_name).style.color = oldTarget_text;
       }
 }
+var checkRunScroll = false;
+// Function to run when scrolling through the 'content-pros' class
+function handleScroll() {
+  const contentPros = document.querySelector('.content-pros');
+  const contentProsTop = contentPros.offsetTop;
+  const contentProsBottom = contentProsTop + contentPros.clientHeight;
+  const scrollY = window.scrollY || window.pageYOffset;
+
+  // Check if the 'content-pros' section is in the viewport
+  if (scrollY >= contentProsTop && scrollY <= contentProsBottom && checkRunScroll == false) {
+      // Your code to run when scrolling through 'content-pros' goes here
+      console.log('Scrolled through .content-pros');
+      checkRunScroll = true;
+  }
+}
+
+// Add an event listener for the 'scroll' event
+window.addEventListener('scroll', handleScroll);
+
+function setRandomPosition(element) {
+  const maxWidth = document.querySelector('.aqi-grid-pros').clientWidth;
+  const maxHeight = document.querySelector('.aqi-grid-pros').clientHeight-50;
+  const left = Math.random() * (maxWidth - element.clientWidth) + 'px';
+  const top = Math.random() * (maxHeight - element.clientHeight) + 'px';
+  
+  element.style.left = left;
+  element.style.top = top;
+}
+function setRandomAQInumber(element){
+  const setAQI = document.getElementsByClassName("blinking-text");
+  for(let j of setAQI){
+      const min = 50;
+      const max = 120;
+      const randomInteger = Math.floor(Math.random() * (max - min + 1)) + min;
+      element.innerHTML = `<h4>AQI</h4><span class="aqi-show">${randomInteger}</span>`;
+  }
+}
+
+
+
+function updatePositions(animationNum) {
+  const textElements = document.querySelectorAll('.animation_num_' + animationNum);
+ 
+  textElements.forEach((element) => {
+      setRandomPosition(element);
+      setRandomAQInumber(element);
+  });
+}
+
+
+// Initial positioning for each animation group
+updatePositions(1);
+updatePositions(2);
+updatePositions(3);
+updatePositions(4);
+
+// Periodically update positions for each animation group
+setInterval(() => updatePositions(1), 2000);
+setInterval(() => updatePositions(2), 3000);
+setInterval(() => updatePositions(3), 2500);
+setInterval(() => updatePositions(4), 4000);
 
